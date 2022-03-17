9cc3574bc008ad32a5a31f9e9afc969a23c70f21
happykongner <eosbelly@protonmail.com>
2022-03-17 23:48:18 +0800

    2

--- test1.js
+++ test1.js
@@ -1,20 +1,40 @@
-function descendingOrder(n){
-    return parseInt(String(n).split('').sort().reverse().join(''))
+function descendingOrder(n) {
+  return parseInt(String(n).split("").sort().reverse().join(""));
+}
+
+function descendingOrder(n) {
+  return +(n + "")
+    .split("")
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .join("");
+}
+
+function descendingOrder(n) {
+  return parseInt(n.toString().split("").sort().reverse().join(""), 10);
+}
+
+function descendingOrder(n) {
+  return parseInt(n.toString().split("").sort().reverse().join(""));
+}
+
+function descendingOrder(n) {
+  // let arr =Array.from(String(n), Number);
+  let arr = n.toString().split("").map(Number);
+
+  if (arr == null || arr.length < 2) {
+    return n;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    let maxValueIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      maxValueIndex = arr[j] > arr[maxValueIndex] ? j : maxValueIndex;
+    }
+    let temp = arr[i];
+    arr[i] = arr[maxValueIndex];
+    arr[maxValueIndex] = temp;
   }
 
-
-
-
-  function descendingOrder(n){
-    return +(n + '').split('').sort(function(a,b){ return b - a }).join('');
-  }
-
-
-  function descendingOrder(n){
-    return parseInt(n.toString().split('').sort().reverse().join(''), 10);
-  }
-
-
-  function descendingOrder(n) {
-    return parseInt(n.toString().split("").sort().reverse().join(""));
-  }
+  return parseInt(arr.join(""), 10);
+}
